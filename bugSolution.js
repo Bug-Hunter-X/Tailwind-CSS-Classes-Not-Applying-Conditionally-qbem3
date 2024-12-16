ec53067675dec78b434ddd98b18de9ec2922ec10
BugHunter-X <askzenai@gmail.com>
2024-12-16 13:54:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+```javascript
+// solution to the problem
+<div className={`${styles.container} ${isDarkMode ? 'dark' : ''}`}> 
+  <h1 className="text-3xl font-bold">This is a heading</h1>
+</div>
+```
